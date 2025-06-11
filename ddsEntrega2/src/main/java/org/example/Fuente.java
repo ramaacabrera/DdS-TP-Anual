42f292653de4dcc4fd09872c20a7c07243218743
lucasbogadocr7 <lubogado@frba.utn.edu.ar>
2025-06-11 17:40:28 -0300

    Fuente estatica?

--- ddsEntrega2/src/main/java/org/example/Fuente.java
+++ ddsEntrega2/src/main/java/org/example/Fuente.java
@@ -1,4 +1,5 @@
 package org.example;
 
-public class Fuente {
+public abstract class Fuente {
+
 }
--- ddsEntrega2/src/main/java/org/example/FuenteEstatica.java
+++ ddsEntrega2/src/main/java/org/example/FuenteEstatica.java
@@ -1,4 +1,23 @@
 package org.example;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
-public class FuenteEstatica {
+public class FuenteEstatica extends Fuente {
+
+    private String rutaArchivoCSV;
+
+    public FuenteEstatica(String rutaArchivoCSV) {
+        this.rutaArchivoCSV = rutaArchivoCSV;
+    }
+
+    @Override
+    public List<Hecho> obtenerHechos() {
+        // Leer el CSV y convertir cada línea en un Hecho??
+        // USAR CONEXION CSV Q PUSIMOS EN EL D DE CLASES
+        // this.conexion.obtenerHecho();
+
+        return new ArrayList<>(); //
+    }
 }
+
