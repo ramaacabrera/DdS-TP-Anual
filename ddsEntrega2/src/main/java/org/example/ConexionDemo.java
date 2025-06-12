de84376ceb6c198be0d72daf5c2884dc127dda0d
Joaquin7278 <idiartjoaquin@gmail.com>
2025-06-12 16:05:55 -0300

    Completo funciones con patron Singleton

--- ddsEntrega2/src/main/java/org/example/ConexionDemo.java
+++ ddsEntrega2/src/main/java/org/example/ConexionDemo.java
@@ -1,11 +1,24 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class ConexionDemo extends Conexion{
-    private ConexionDemo instancia;
+public class ConexionDemo extends Conexion {
 
-    @Override
-    public List<Hecho> obtenerHechos(Hecho hecho) {
+    private static ConexionDemo instance = null;
+
+    private ConexionDemo() {}
+
+    public static ConexionDemo GetInstance()
+    {
+        if (instance == null)
+            instance = new ConexionDemo();
+        return instance;
+    }
+
+    public List<Hecho> obtenerHechos() {
+        // implementar
+        return new ArrayList<Hecho>();
     }
 }
+
