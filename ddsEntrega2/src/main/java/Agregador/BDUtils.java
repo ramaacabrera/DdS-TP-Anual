12377c8618a4c2fd71147a00b4bfed889d2c5dd2
Aylen Chazarreta <aychazarreta@frba.utn.edu.ar>
2025-10-13 15:27:47 -0300

    avances bd dinamica

---
+++ ddsEntrega2/src/main/java/Agregador/BDUtils.java
@@ -0,0 +1,42 @@
+package Agregador;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class BDUtils {
+//    private static final EntityManagerFactory factory;
+
+//    static {
+//        factory = Persistence.createEntityManagerFactory("demo-hibernate-PU");
+//    }
+
+//    public static EntityManager getEntityManager() {
+//        EntityManager em = factory.createEntityManager();
+//        return em;
+//    }
+
+    private static final String PERSISTENCE_UNIT_NAME = "agregador-hibernate-PU";
+
+    public static void comenzarTransaccion(EntityManager em) {
+        EntityTransaction tx = em.getTransaction();
+        if (!tx.isActive()) {
+            tx.begin();
+        }
+    }
+
+    public static void commit(EntityManager em) {
+        EntityTransaction tx = em.getTransaction();
+        if (tx.isActive()) {
+            tx.commit();
+        }
+    }
+
+    public static void rollback(EntityManager em) {
+        EntityTransaction tx = em.getTransaction();
+        if (tx.isActive()) {
+            tx.rollback();
+        }
+    }
+}
---
+++ ddsEntrega2/src/main/java/CargadorDinamica/BDUtilsDinamico.java
@@ -0,0 +1,31 @@
+package CargadorDinamica;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+public class BDUtilsDinamico {
+
+    private static final String PERSISTENCE_UNIT_NAME = "dinamico-hibernate-PU";
+
+    public static void comenzarTransaccion(EntityManager em) {
+        EntityTransaction tx = em.getTransaction();
+        if (!tx.isActive()) {
+            tx.begin();
+        }
+    }
+
+    public static void commit(EntityManager em) {
+        EntityTransaction tx = em.getTransaction();
+        if (tx.isActive()) {
+            tx.commit();
+        }
+    }
+
+    public static void rollback(EntityManager em) {
+        EntityTransaction tx = em.getTransaction();
+        if (tx.isActive()) {
+            tx.rollback();
+        }
+    }
+
+}
