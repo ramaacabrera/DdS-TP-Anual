b101a089f26193579ee1acfd063fbb731144197b
Ramiro Cabrera <ramacabrera16@gmail.com>
2025-10-25 22:49:08 -0300

    mis famosos avances (creo que anda toda la parte de agregar hechos)

---
+++ DdS/cargadorDinamico/src/main/java/cargadorDinamico/DinamicaDto/Hecho_D_DTO.java
@@ -0,0 +1,55 @@
+package cargadorDinamico.DinamicaDto;
+
+import cargadorDinamico.Dominio.HechosYColeccionesD.ContenidoMultimedia_D;
+import cargadorDinamico.Dominio.HechosYColeccionesD.Etiqueta_D;
+import cargadorDinamico.Dominio.HechosYColeccionesD.Ubicacion_D;
+import cargadorDinamico.Dominio.Usuario.Usuario_D;
+
+import java.util.Date;
+import java.util.List;
+
+public class Hecho_D_DTO {
+    private String titulo;
+    private String descripcion;
+    private String categoria;
+    private Ubicacion_D ubicacion;
+    private Date fechaDeAcontecimiento;
+    private Date fechaDeCarga;
+    private Usuario_D contribuyente;
+    private List<Etiqueta_D> etiquetas;
+    private List<ContenidoMultimedia_D> contenidoMultimedia;
+
+    public Hecho_D_DTO() {}
+
+    public Hecho_D_DTO(String titulo, String descripcion, String categoria, Ubicacion_D ubicacion, Date fechaDeAcontecimiento
+                    /*Date fechaDeCarga*/, Usuario_D contribuyente, List<Etiqueta_D> etiquetas, List<ContenidoMultimedia_D> contenidoMultimedia) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.categoria = categoria;
+        this.ubicacion = ubicacion;
+        this.fechaDeAcontecimiento = fechaDeAcontecimiento;
+        //this.fechaDeCarga = fechaDeCarga;
+        this.contribuyente = contribuyente;
+        this.etiquetas = etiquetas;
+        this.contenidoMultimedia = contenidoMultimedia;
+    }
+
+    public String getTitulo() { return titulo;}
+    public String getDescripcion() { return descripcion;}
+    public String getCategoria() { return categoria;}
+    public Ubicacion_D getUbicacion() { return ubicacion;}
+    public Date getFechaDeAcontecimiento() { return fechaDeAcontecimiento;}
+    //public Date getFechaDeCarga() { return fechaDeCarga;}
+    public Usuario_D getContribuyente() { return contribuyente;}
+    public List<Etiqueta_D> getEtiquetas() { return etiquetas;}
+    public List<ContenidoMultimedia_D> getContenidoMultimedia() { return contenidoMultimedia; }
+
+    public void setTitulo(String titulo){ this.titulo = titulo;}
+    public void setDescripcion(String descripcion){ this.descripcion = descripcion;}
+    public void setCategoria(String categoria){ this.categoria = categoria;}
+    public void setUbicacion(Ubicacion_D ubicacion) {this.ubicacion = ubicacion;}
+    public void setFechaDeAcontecimiento(Date fechaDeAcontecimiento){ this.fechaDeAcontecimiento = fechaDeAcontecimiento;}
+    public void setContribuyente(Usuario_D contribuyente){ this.contribuyente = contribuyente;}
+    public void setEtiquetas(List<Etiqueta_D> etiquetas) {this.etiquetas = etiquetas;}
+    public void setContenidoMultimedia(List<ContenidoMultimedia_D> contenidoMultimedia) {this.contenidoMultimedia = contenidoMultimedia;}
+}
---
+++ DdS/utils/src/main/java/utils/Dominio/HechosYColecciones/Ubicacion.java
@@ -0,0 +1,43 @@
+package utils.Dominio.HechosYColecciones;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ubicacion")
+public class Ubicacion {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Type(type = "uuid-char")
+    @Column(name = "id_ubicacion", length = 36 , updatable = false, nullable = false)
+    private UUID id_ubicacion;
+
+    private double latitud;
+    private double longitud;
+
+    public Ubicacion(){}
+
+    public Ubicacion(double latitud, double longitud) {
+        this.latitud = latitud;
+        this.longitud = longitud;
+    }
+
+    public double getLatitud() { return latitud; }
+    public double getLongitud() { return longitud; }
+    public UUID getId_ubicacion() {return id_ubicacion;}
+
+    public void setLatitud(double latitud) { this.latitud = latitud; }
+    public void setLongitud(double longitud) { this.longitud = longitud; }
+    public void setId_ubicacion(UUID id_ubicacion) {this.id_ubicacion = id_ubicacion;}
+}
+
